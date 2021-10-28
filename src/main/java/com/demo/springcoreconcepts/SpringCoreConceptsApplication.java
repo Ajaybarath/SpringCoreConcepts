574ac12d04690f4743c35ffa3a3c9411174fc2b0
ajaybarath <ajaybarath3@gmail.com>
2021-10-28 22:57:06 +0530

    Added: initialisation of employeeBean to call methods

--- src/main/java/com/demo/springcoreconcepts/SpringCoreConceptsApplication.java
+++ src/main/java/com/demo/springcoreconcepts/SpringCoreConceptsApplication.java
@@ -1,6 +1,7 @@
 package com.demo.springcoreconcepts;
 
 import com.demo.springcoreconcepts.component.DemoBean;
+import com.demo.springcoreconcepts.component.EmployeeBean;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,6 +19,10 @@ public class SpringCoreConceptsApplication {
         ApplicationContext context = (ApplicationContext) SpringApplication.run(SpringCoreConceptsApplication.class, args);
         logger.debug("Checking context : {}", context.getBean(DemoBean.class));
         logger.debug("\n*** Example Using @Autowire annotation on property ***");
+        EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+        employeeBean.setEid(104);
+        employeeBean.setEname("Ajay");
+        employeeBean.showEmployeeDetails();
     }
 
 }
