bae0be95a5e4e001cc6bfd78aa92bcf41acb16ce
ajaybarath <ajaybarath3@gmail.com>
2021-10-28 22:39:30 +0530

    Added: logger to print the logs

--- src/main/java/com/demo/springcoreconcepts/SpringCoreConceptsApplication.java
+++ src/main/java/com/demo/springcoreconcepts/SpringCoreConceptsApplication.java
@@ -1,13 +1,23 @@
 package com.demo.springcoreconcepts;
 
+import com.demo.springcoreconcepts.component.DemoBean;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreConceptsApplication {
 
+    public static final Logger logger = (Logger) LoggerFactory.getLogger(SpringCoreConceptsApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringCoreConceptsApplication.class, args);
+        logger.debug("Welcome to Spring Concepts Demo");
+        ApplicationContext context = (ApplicationContext) SpringApplication.run(SpringCoreConceptsApplication.class, args);
+        logger.debug("Checking context : {}", context.getBean(DemoBean.class));
+        logger.debug("\n*** Example Using @Autowire annotation on property ***");
     }
 
 }
